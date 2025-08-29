65e23111863174edbe2acdd3080d0d7bd0a5b704
Gaurav Verma <briskgaurav@gmail.com>
2025-08-29 23:11:13 +0530

    Loading Load

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,34 @@
 'use client'
-import About from '@/components/Home/About'
-import Footer from '@/components/Home/Footer'
-import FooterMobile from '@/components/Home/FooterMobile'
-import HelpingBrand from '@/components/Home/HelpingBrand'
-import Hero from '@/components/Home/Hero'
-import IdeasInMotion from '@/components/Home/IdeasInMotion'
-import VideoSection from '@/components/Home/VideoSection'
-import Works from '@/components/Home/Works'
-import WorksMobile from '@/components/Home/WorksMobile'
-import NavBar from '@/components/NavBar'
+import dynamic from 'next/dynamic'
+import React, { Suspense } from 'react'
 import UseTablet from '@/components/Responsive/UseTablet'
-import React from 'react'
+import NavBar from '@/components/NavBar'
+import Hero from '@/components/Home/Hero'
+
+const About = dynamic(() => import('@/components/Home/About'), {
+  loading: () => <div>Loading...</div>
+})
+const Works = dynamic(() => import('@/components/Home/Works'), {
+  loading: () => <div>Loading...</div>
+})
+const WorksMobile = dynamic(() => import('@/components/Home/WorksMobile'), {
+  loading: () => <div>Loading...</div>
+})
+const HelpingBrand = dynamic(() => import('@/components/Home/HelpingBrand'), {
+  loading: () => <div>Loading...</div>
+})
+const VideoSection = dynamic(() => import('@/components/Home/VideoSection'), {
+  loading: () => <div>Loading...</div>
+})
+const IdeasInMotion = dynamic(() => import('@/components/Home/IdeasInMotion'), {
+  loading: () => <div>Loading...</div>
+})
+const Footer = dynamic(() => import('@/components/Home/Footer'), {
+  loading: () => <div>Loading...</div>
+})
+const FooterMobile = dynamic(() => import('@/components/Home/FooterMobile'), {
+  loading: () => <div>Loading...</div>
+})
 
 export default function page() {
   const isTablet = UseTablet();
@@ -18,12 +36,14 @@ export default function page() {
     <>
       <NavBar />
       <Hero />
-      {!isTablet && <About />}
-      {isTablet ? <WorksMobile /> : <Works />}
-      <HelpingBrand />
-      <VideoSection />
-      <IdeasInMotion />
-      {isTablet ? <FooterMobile /> : <Footer />}
+      <Suspense fallback={<div>Loading...</div>}>
+        {!isTablet && <About />}
+        {isTablet ? <WorksMobile /> : <Works />}
+        <HelpingBrand />
+        <VideoSection />
+        <IdeasInMotion />
+        {isTablet ? <FooterMobile /> : <Footer />}
+      </Suspense>
     </>
   )
 }
